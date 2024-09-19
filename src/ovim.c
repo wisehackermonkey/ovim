471c2d196487a6f7bba939ba3eed6a71e466c296
wisehackermonkey <oranbusiness@gmail.com>
2024-09-19 17:06:06 -0600

    no erors base

--- src/ovim.c
+++ src/ovim.c
@@ -1,6 +1,51 @@
+#include <ncurses.h>
 #include <stdio.h>
-  
-  int main() {
-    printf("ovim Works!\n");
-    return 0;
-  }
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <time.h>
+
+char ch;
+char arrow;
+int x = 0;
+int y = 0;
+struct termios original_settings;
+
+int main(int argc, char **argv)
+{
+  printf("ovim Works!\n");
+  int ch;
+  int x = 0;
+  int y = 0;
+  initscr();
+
+  keypad(stdscr, TRUE);
+  do
+  {
+    ch = getch();
+    switch (ch)
+    {
+    case KEY_DOWN:
+      printf("down");
+      x++;
+      break;
+    case KEY_UP:
+      x--;
+      break;
+    case KEY_LEFT:
+      y++;
+      break;
+    case KEY_RIGHT:
+      y--;
+    default:
+      char str[2] = { (char)ch, '\0' };
+      addstr(str);
+      break;
+    }
+    refresh();
+  } while (ch != '~');
+
+  endwin();
+
+  return EXIT_SUCCESS;
+}
